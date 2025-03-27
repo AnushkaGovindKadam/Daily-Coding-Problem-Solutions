6c06113af7bb0a4889a578ec6a53ba326e82fa06
Anushka Kadam <160218768+AnushkaGovindKadam@users.noreply.github.com>
2025-03-27 12:03:57 +0530

    Update 001.py

--- Solutions/001.py
+++ Solutions/001.py
@@ -4,8 +4,8 @@ Problem:
 Given a list of numbers, return whether any two sums to k. For example, given
 [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-Bonus: Can you do this in one pass?
 """
+
 
 from typing import List
 
